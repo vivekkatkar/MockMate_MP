734541c7d343c17d407734d1bd0223485a39eafb
Vivek Katkar <102805058+vivekkatkar@users.noreply.github.com>
2024-11-07 09:39:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'public'  // Matches Vercel's expected output directory
-  }
+  plugins: [react()],
 })
