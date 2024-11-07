cae40afb40782cf6bc893ecc87ad79a01dfea876
vivekkatkar <katkarvivek0@gmail.com>
2024-11-07 10:19:38 +0530

    Solved Cors error

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const {InsertResume} = require("./Database/index.js")
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 
 const parserRouter = require("./Models/ResumeParser");
 const questionsRouter = require("./Models/QuestionGeneration");
